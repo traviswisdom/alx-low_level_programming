a498da7b73359bfa02a7741a84c2ca7f3343ee8e
travidwisdom <wisdomtravisasase649002@yahoo.com>
2023-03-22 09:09:54 -0700

    task 0x02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - wrietes the characters
+ *
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+	char k = 'a';
+
+	while (i >= 'x')
+	{
+		_putchar(k);
+		k--;
+	}
+	_putchar('\n');
+}
