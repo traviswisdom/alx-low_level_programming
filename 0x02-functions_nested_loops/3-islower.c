70e6fbb43092e32a0aba843d0b0f8043d2faff06
travidwisdom <wisdomtravisasase649002@yahoo.com>
2023-03-22 12:53:03 -0700

    code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+
+#include <unistd.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (success)
+ * @c: The character to check
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
