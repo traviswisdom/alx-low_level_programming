56bbfdd529dfbb67db1e496114817b14da3bf464
travidwisdom <wisdomtravisasase649002@yahoo.com>
2023-03-22 21:54:54 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/**
+ * print_to_98 - entry
+ * return: always important
+ */
+void print_to_98(int n)
+{
+	for ( int i = n ; i <= 98 ; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (n);
+}
+
