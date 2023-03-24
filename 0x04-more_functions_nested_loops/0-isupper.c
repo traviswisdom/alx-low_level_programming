01d59d355f9923f19ae08bb8931e44ac5eb10085
travidwisdom <wisdomtravisasase649002@yahoo.com>
2023-03-24 16:09:38 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+/** 
+ * _isupper - Entry 
+ * Retrn : Always 0  success
+ * @c: The input 
+ */
+
+int _isuper (int c)
+
+	if (c >= 'A'  && c <= 'z'){
+		return (1);
+	}
+	else
+	{
+	       	return (0);
+	}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x04-more_functions_nested_loops 
+my Readme file not enjo
