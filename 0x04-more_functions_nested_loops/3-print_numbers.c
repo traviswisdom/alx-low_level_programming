098bd2638c3e665166da2ac67f3bcf595958b993
travidwisdom <wisdomtravisasase649002@yahoo.com>
2023-03-24 22:26:54 -0700

    betty style

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
- *
- * Return: 0-9 followed by newline
+ * print_to_98 - entry
+ * return: always important
  */
-void print_numbers(void)
+void print_to_98(int n)
 {
-	int i;
-
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+	for ( int i = n ; i <= 98 ; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (n);
 }
-
